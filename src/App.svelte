c2bca3819e37f81f49e7d9ac678d78094f7668a2
Europrimus <dev@europrimus.ninja>
2022-01-23 21:59:10 +0100

    display photos from server

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,77 @@
 <script>
-	export let name;
+	import { onMount } from 'svelte';
+	export let img;
+
+	async function fetchImg(){
+		let url = img.url + getCurrentYear();
+		const request = await fetch(url, {
+			method: 'GET'
+		})
+		return await request.json()
+	}
+
+	let currentImg = 0;
+	let fetching = fetchImg();
+
+	async function sheduleSwitchImg(time=1500){
+		setTimeout(()=>{
+			switchImg()
+		},time)
+	}
+
+	function switchImg(){
+		fetching.then((imgs)=>{
+			if(imgs.length == 0 || currentImg == imgs.length - 1){
+				console.log("next dir");
+				loadNextYear();
+			}else{
+				console.log("next img");
+				let currentImgElement = document.getElementById('currentImg')
+				currentImg = ++currentImg % imgs.length;
+				currentImgElement.src = imgs[currentImg].url;
+				sheduleSwitchImg();
+			}
+		})
+	}
+
+	function getCurrentYear(){
+		const queryString = document.location.search;
+		const params = new URLSearchParams(queryString);
+		let year = parseInt(params.get("year"));
+		if(isNaN(year)){
+			year = img.dirs.start;
+		}
+		return year;
+	}
+
+	function loadNextYear(){
+		const params = new URLSearchParams();
+		const currentYear = getCurrentYear();
+		if(currentYear < img.dirs.end){
+			params.set("year",currentYear + 1)
+			const url = document.location.origin + "?" + params.toString();
+			document.location.replace(url);
+		}else{
+			console.log("last year : END");
+		}
+	}
+
+	onMount(
+		sheduleSwitchImg()
+	);
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>{getCurrentYear()}</h1>
+	{#await fetching}
+		Chargement ...
+	{:then imgs}
+		<img src="{imgs[currentImg].url}" alt="photo" id="currentImg">
+	{:catch error}
+		Erreur de chargement.
+	{/await}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
+		img: {
+			url: 'http://img.europrimus.ninja/1952-2022/',
+			dirs: {
+				start: 1952,
+				end: 2022
+			}
+		}
 	}
 });
 
